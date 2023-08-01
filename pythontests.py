aa377b532d199b04af65682343854e2b9f0e58a0
Andre Paez <112267180+apaezcastro@users.noreply.github.com>
2023-08-01 19:43:48 -0400

    culvers order

--- pythontests.py
+++ pythontests.py
@@ -1,4 +1,4 @@
-name = input("What is your name? ")
-print("...\n")
-
-print("Hello " + name + ", I hope you are well")
+name = input("Hello Welcome to Culvers, can I have a name for the order? ")
+print("Hello " + name )
+food = input("What would you like to eat today? ")
+print("Okay great, your " + food + " will be be ready in 5 minutes, here is a table number, we will bring it right out to you.")
