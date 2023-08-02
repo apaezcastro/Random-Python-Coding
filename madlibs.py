c0d709cde376e0d8f115e9d89586a0faa89ecd55
Andre Paez <112267180+apaezcastro@users.noreply.github.com>
2023-08-02 09:19:45 -0400

    madlibs project

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+adjective = input("Write an adjective: ")
+print ("\n")
+adjective2 = input("Write a second ajective: ")
+print ("\n")
+bird = input("Name a type of bird: ")
+print ("\n")
+room = input("Name a room in the house: ")
+print ("\n")
+
+
+print("It was a " + adjective + """, cold November day.\n
+I woke up to the """ + adjective2 + " smell of " + bird + """ roasting\n
+in the """ + room + " downstairs.")
