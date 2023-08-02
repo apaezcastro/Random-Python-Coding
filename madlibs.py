df509d9d4c681c56a943455bed1ec081be39e7d3
Andre Paez <112267180+apaezcastro@users.noreply.github.com>
2023-08-02 09:29:20 -0400

    added a few more lines, and fixed syntax errors

--- madlibs.py
+++ madlibs.py
@@ -6,8 +6,12 @@ bird = input("Name a type of bird: ")
 print ("\n")
 room = input("Name a room in the house: ")
 print ("\n")
-
+pastverb = input("Write a verb in past tense: ")
+print ("\n")
+verb = input("Write another verb: ")
+print ("\n")
 
 print("It was a " + adjective + """, cold November day.\n
 I woke up to the """ + adjective2 + " smell of " + bird + """ roasting\n
-in the """ + room + " downstairs.")
+in the """ + room + """ downstairs. I """ + pastverb + """ down the stairs to see \n
+if I could help """ + verb + " the dinner.")
