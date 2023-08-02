e23194fcf73f137b80bbe1689afe0b1daaabbbd2
Andre Paez <112267180+apaezcastro@users.noreply.github.com>
2023-08-02 10:10:25 -0400

    added some more math and went over modulo

--- maths.py
+++ maths.py
@@ -1,1 +1,14 @@
 print(1 + 3)
+print(2*12)
+print(2**3) #calculating power is the same a multiplying by its self that why it is this
+print(15/3) #normal division always returns a float
+print(15%3) #get the remainder/mod/modulo, remember because percentage has 0 on top and bottom
+print(15//3) #floor division
+
+print("\n")
+
+print(1%6) #understanding modulo operator
+print(1/6)
+print(1//6)
+print(6%1)
+print(6//1)
