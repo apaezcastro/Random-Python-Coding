ee9d73bd101eccbae2c8967b25f63fb7501a7352
Andre Paez-Castro <112267180+apaezcastro@users.noreply.github.com>
2023-08-14 22:11:33 -0400

    Update CodingProject1.py
    game

--- CodingProject1.py
+++ CodingProject1.py
@@ -1,5 +1,5 @@
-name = input("Enter name: ")
+name = input("Select your character name: ")
 if name.lower() == "andre":
     input("Hello Sire, What can I do for you today? ")
 else:
-    print("Hello Stranger")
+    print("Hello Stranger, welcome to this stange land")
