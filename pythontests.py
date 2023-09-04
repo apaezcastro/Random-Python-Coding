388261211c5a070add9076886acfb55d2e2a7350
Andre Paez <112267180+apaezcastro@users.noreply.github.com>
2023-09-04 19:26:03 -0400

    listsssss

--- pythontests.py
+++ pythontests.py
@@ -1,10 +1,30 @@
-# this is for learning if else syntax
+# this is gonna be for more notes other than math which contains all kinds of notes
 
-x = 3
+def nl(): # this prints out a new line in fewer characters
+    print('\n')
+nl()
 
-if x == 1:
-    print("Hello World")
-elif x == 2:
-    print("Hello Two")
-else:
-    print("not Hello World")
+#LISTS - Have brackets [] inside lists are items
+
+movies = ['In Brudge', 'Inglorious Basterds', 'John Wick']
+carla_movies = ['Star Wars', 'John Wick']
+print(movies[1]) #returnes second item in list
+print(movies[0]) #returns 1st item in list
+print(movies[0:2]) # returnes first item listed, butand goes up to the last one but does not print it
+print(movies[:1])
+print(movies[2:])
+nl()
+nl()
+#methods for lists .pop() empty removes last, .append(), .insert() 
+def lastmovieinlist(): #this fun this function removes last element from list and returns it
+    lastmv = movies.pop()
+    return lastmv
+
+movies.insert(0, lastmovieinlist()) #this puts john wick at the front
+print(movies)
+#list can also be concatenated
+print(movies + carla_movies)
+ourmovies = movies + carla_movies
+print(ourmovies)
+print(movies + ['lol'])
+# also can do lists inside of lists 2d lists
