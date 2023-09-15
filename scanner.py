8d1d528a2afe6fd59c8a8e3b73383a210fca9d41
apaezcastro <112267180+apaezcastro@users.noreply.github.com>
2023-09-15 14:36:03 -0400

    sockets

--- scanner.py
+++ scanner.py
@@ -21,4 +21,21 @@ print('Time started: ',str(datetime.now()))
 print('-'*50)
 
 try:
-    for port in range(50,85)
+    for port in range(50,85):
+        s = socket.sockett(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = s.connect_ex((target,port))
+        if result ==0:
+            print(f'Port {port} is open')
+            s.close()
+
+except KeyboardInterrupt:
+    print('\nExiting program.')
+    sys.exit()
+
+except socket.gaierror:
+    print('Hostname could not be resolved.')
+
+except socket.error:
+    print('Could not connect to server.')
+    sys.exit()
