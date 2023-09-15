b6e3a074766ab8c713099551aff1e105905ccf5a
apaezcastro <112267180+apaezcastro@users.noreply.github.com>
2023-09-14 22:32:11 -0400

    learning about importing modules

---
+++ importing.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+
+#IMPORTING - Importing is important
+import sys #system functions and parameters
+from datetime import datetime as dt #import a specific funtion from a module as gives alias
+
+print(sys.version) #from sys prints python version
+print(dt.now()) #from datetime prints current date and time
